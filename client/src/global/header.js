6e65834c9d99c5cea055f548ee714f6d7f78ed0c
habenzy <rstauss@burlingtoncodeacademy.com>
2022-09-28 12:26:21 -0400

    neon nav

--- client/src/global/header.js
+++ client/src/global/header.js
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
-  return(
+  return (
     <header>
       <div id="title-container">
-        <h1 className="neon"><span className="flicker">B</span>ob Stauss</h1>
-        <div id="h-subtitle" >
+        <h1 className="neon">
+          <span className="flicker">B</span>ob Stauss
+        </h1>
+        <div id="h-subtitle">
           <h4 className="neon">Programmer</h4>
           <div class="dot"></div>
           <h4 className="neon">Artist</h4>
@@ -16,10 +18,12 @@ export default function Header(props) {
 
       <nav>
         <Link to="/">Home</Link>
-        <Link to="projects">Projects</Link>
-        <Link to="gallery">Art</Link>
-        <Link to="nerdery">Nerdy Things</Link>
+        <div id="page-nav">
+          <Link to="gallery">Art Gallery</Link>
+          <Link to="projects">Projects</Link>
+          <Link to="nerdery">Nerdy Things</Link>
+        </div>
       </nav>
     </header>
-  )
+  );
 }
