7b4010ef0b570f2a57577a4cb92bb2f81afe1609
habenzy <rstauss@burlingtoncodeacademy.com>
2022-09-28 12:01:43 -0400

    flickering neon text

--- client/src/global/header.js
+++ client/src/global/header.js
@@ -4,13 +4,13 @@ export default function Header(props) {
   return(
     <header>
       <div id="title-container">
-        <h1>Bob Stauss</h1>
-        <div id="h-subtitle">
-          <h4>Programmer</h4>
-          <div class="dot"></div>
-          <h4>Artist</h4>
-          <div class="dot"></div>
-          <h4>Pretty Cool Dude</h4>
+        <h1 className="neon"><span className="flicker">B</span>ob Stauss</h1>
+        <div id="h-subtitle" >
+          <h4 className="neon">Programmer</h4>
+          <div class="dot neon"></div>
+          <h4 className="neon">Artist</h4>
+          <div class="dot neon"></div>
+          <h4 className="neon">Pretty Cool Dude</h4>
         </div>
       </div>
 
