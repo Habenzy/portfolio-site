c63ded04a4b342b5ff1fa55ac61961d5776ff724
habenzy <rstauss@burlingtoncodeacademy.com>
2022-09-28 12:05:59 -0400

    updated nav locations

--- client/src/global/header.js
+++ client/src/global/header.js
@@ -7,16 +7,15 @@ export default function Header(props) {
         <h1 className="neon"><span className="flicker">B</span>ob Stauss</h1>
         <div id="h-subtitle" >
           <h4 className="neon">Programmer</h4>
-          <div class="dot neon"></div>
+          <div class="dot"></div>
           <h4 className="neon">Artist</h4>
-          <div class="dot neon"></div>
+          <div class="dot"></div>
           <h4 className="neon">Pretty Cool Dude</h4>
         </div>
       </div>
 
       <nav>
         <Link to="/">Home</Link>
-        <Link to="about">About me</Link>
         <Link to="projects">Projects</Link>
         <Link to="gallery">Art</Link>
         <Link to="nerdery">Nerdy Things</Link>
