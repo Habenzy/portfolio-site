aa199231d8ba715bf50fb848d16680f0ef423647
habenzy <rstauss@burlingtoncodeacademy.com>
2022-09-29 10:11:48 -0400

    typo

--- client/src/nerdy_shit/nerdery.js
+++ client/src/nerdy_shit/nerdery.js
@@ -8,22 +8,41 @@ export default function Nerdery(props) {
   return (
     <div>
       <p>
-        Welcome to my hobby page. In addition to my many other excellent qualities, I'm a huge nerd, and play Warhammer 40k, and D&D (I DM one group, and play in another). Enjoy some pictures of maps, minis, and more!
+        Welcome to my hobby page. In addition to my many other excellent
+        qualities, I'm a huge nerd, and play Warhammer 40k, and D&D (I DM one
+        group, and play in another). Enjoy some pictures of maps, minis, and
+        more!
       </p>
-      <p>If you're curious about my homebrew setting you can check out the lore <a href="https://www.worldanvil.com/w/the-fractured-hub-habenzy" target="_blank">here</a></p>
+      <p>
+        If you're curious about my homebrew setting you can check out the lore{" "}
+        <a
+          href="https://www.worldanvil.com/w/the-fractured-hub-habenzy"
+          target="_blank"
+        >
+          here
+        </a>
+      </p>
       <div id="dandd">
         <div class="gallery">
-          {dndStuff.map((pic, index) => (
-            <GalleryItem source={pic.src} title={pic.title} key={index} />
-          ))}
+          {dndStuff.length ? (
+            dndStuff.map((pic, index) => (
+              <GalleryItem source={pic.src} title={pic.title} key={index} />
+            ))
+          ) : (
+            <p className="construction">Pictures coming soon!</p>
+          )}
         </div>
       </div>
 
       <div id="warhammer">
         <div class="gallery">
-          {wh40kStuff.map((pic, index) => (
-            <GalleryItem source={pic.src} title={pic.title} key={index} />
-          ))}
+          {wh40kStuff.length ? (
+            wh40kStuff.map((pic, index) => (
+              <GalleryItem source={pic.src} title={pic.title} key={index} />
+            ))
+          ) : (
+            <p className="construction">Pictures coming soon!</p>
+          )}
         </div>
       </div>
     </div>
