868ba2bb7b37b8874d52b37c4f580b1e015c542f
Robert Stauss <robertrstauss@gmail.com>
2022-10-12 10:10:42 -0400

    basic info form

--- client/src/db_management/UploadPortal.js
+++ client/src/db_management/UploadPortal.js
@@ -1,7 +1,14 @@
 export default function Uploader(props) {
-  return (
-    <form>
 
+  let uploadEntry = (evt) => {
+    evt.preventDefault()
+  }
+  return (
+    <form onSubmit={uploadEntry}>
+      <input type='file' name="image" />
+      <input type="text" name="title" />
+      <input type="text" name="blurb" />
+      <input type="submit" />
     </form>
   )
 }
