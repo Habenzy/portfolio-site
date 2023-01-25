68307c2aad8ca8cf9c795605311426a99e505c62
habenzy <rstauss@burlingtoncodeacademy.com>
2023-01-25 10:57:19 -0500

    security for login portal

--- client/src/db_management/UploadPortal.js
+++ client/src/db_management/UploadPortal.js
@@ -1,16 +1,33 @@
-import { useState } from "react"
-import { firestore, storage } from "../firebase/firebase_config";
+import { useState, useEffect } from "react";
+import { firestore, storage, auth } from "../firebase/firebase_config";
 import { setDoc, doc } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 export default function Uploader(props) {
-  const [img, setImg] = useState('');
-  const [title, setTitle] = useState('');
-  const [blurb, setBlurb] = useState('');
-  const [gallery, setGallery] = useState('nerdery')
+  const [img, setImg] = useState("");
+  const [title, setTitle] = useState("");
+  const [blurb, setBlurb] = useState("");
+  const [gallery, setGallery] = useState("nerdery");
+  const [user, setUser] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  let uploadEntry = async (evt) => {
-    evt.preventDefault()
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+  });
+
+  const handleSignIn = (evt) => {
+    evt.preventDefault();
+    signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const uploadEntry = async (evt) => {
+    evt.preventDefault();
 
     let galleryRef = ref(storage, gallery);
     console.log(galleryRef);
@@ -46,14 +63,57 @@ export default function Uploader(props) {
         });
       }
     );
-  }
-  
-  return (
+  };
+
+  return user ? (
     <form onSubmit={uploadEntry}>
-      <input type='file' name="image" value={img} onChange={(evt) => setImg(evt.target.value)}/>
-      <input type="text" name="title" placeholder="Title" value={title} onChange={(evt) => setTitle(evt.target.value)}/>
-      <input type="text" name="blurb" placeholder="Blurb" value={blurb} onChange={(evt) => setBlurb(evt.target.value)}/>
+      <input
+        type="file"
+        name="image"
+        value={img}
+        onChange={(evt) => setImg(evt.target.value)}
+      />
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value={title}
+        onChange={(evt) => setTitle(evt.target.value)}
+      />
+      <input
+        type="text"
+        name="blurb"
+        placeholder="Blurb"
+        value={blurb}
+        onChange={(evt) => setBlurb(evt.target.value)}
+      />
       <input type="submit" />
     </form>
-  )
+  ) : (
+    <div>
+      <h3>Log In</h3>
+      <form onSubmit={handleSignIn}>
+        <input
+          type="text"
+          value={email}
+          onChange={(evt) => {
+            setEmail(evt.target.value);
+          }}
+          name="email"
+          placeholder="Email"
+        />
+        <br></br>
+        <input
+          type="password"
+          value={password}
+          onChange={(evt) => {
+            setPassword(evt.target.value);
+          }}
+          name="password"
+          placeholder="password"
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
