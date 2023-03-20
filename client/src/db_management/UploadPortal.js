f1c99132127edbc98aa5b778f8bf0a2d089bbc43
habenzy <rstauss@burlingtoncodeacademy.com>
2023-03-20 09:02:35 -0400

    porting over edit functionality

--- client/src/db_management/UploadPortal.js
+++ client/src/db_management/UploadPortal.js
@@ -4,11 +4,84 @@ import { setDoc, doc } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
+function EditEntry(props) {
+  const [editName, setEditName] = useState("");
+  const [editBlurb, setEditBlurb] = useState("");
+  const [editDate, setEditDate] = useState("");
+console.log("names", editName, props.name)
+console.log('dates')
+  function updateGalleryEntry() {
+    const imageDoc = doc(db, props.gallery, props.name);
+    updateDoc(imageDoc, {
+      name: editName || props.name,
+      blurb: editBlurb || props.blurb,
+      date: editDate || props.date,
+    }).then(res => {
+      alert("update successful")
+    }).catch(err => {
+      alert(`update failed: ${err.message}`)
+    });
+  }
+
+  return (
+    <div>
+      <img className="edit-thumb" src={props.url} alt={props.name} />
+      <form
+        className="edit-images"
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          updateGalleryEntry();
+        }}
+      >
+        <label htmlFor="file-name">Image name: </label>
+        <br></br>
+        <input
+          onChange={(evt) => {
+            setEditName(evt.target.value);
+          }}
+          value={editName}
+          name="file-name"
+          type="text"
+          placeholder={props.name}
+        />
+        <br></br>
+        <label htmlFor="date-taken">Date taken: </label>
+        <br></br>
+        <input
+          onChange={(evt) => {
+            setEditDate(evt.target.value);
+          }}
+          value={editDate}
+          name="date-taken"
+          type="date"
+        />
+        <br></br>
+        <label htmlFor="edit-blurb">
+          Description that will appear under the image:
+        </label>
+        <br></br>
+        <input
+          onChange={(evt) => {
+            setEditBlurb(evt.target.value);
+          }}
+          value={editBlurb}
+          name="edit-blurb"
+          type="text"
+          placeholder={props.blurb}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
 export default function Uploader(props) {
   const [img, setImg] = useState("");
   const [title, setTitle] = useState("");
   const [blurb, setBlurb] = useState("");
+
   const [gallery, setGallery] = useState("nerdery");
+
   const [user, setUser] = useState(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
