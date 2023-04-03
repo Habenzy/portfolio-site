8877a477552ab94a9dc5403ff149ddbb69bb7f42
habenzy <rstauss@burlingtoncodeacademy.com>
2023-04-03 12:21:46 -0400

    gallery desc update for new functionality

--- client/src/nerdy_shit/nerdery.js
+++ client/src/nerdy_shit/nerdery.js
@@ -22,7 +22,7 @@ export default function Nerdery(props) {
         Welcome to my hobby page. In addition to my many other excellent
         qualities, I'm also a huge nerd, and play Warhammer 40k, and D&D (I DM one
         group, and play in another). Enjoy some pictures of maps, minis, and
-        more! That all being said the true purpose of this gallery is to showcase this sweet, scratch built, React image gallery. It's modular! It's repeatable!  It's connected to Firebase, and it's got a hidden admin portal (auth secured) that lets me easily add new images (and will eventually have full CRUD functionality so I can fix spelling mistakes) without having to edit any code, or directly access the database!
+        more! That all being said the true purpose of this gallery is to showcase this sweet, scratch built, React image gallery. It's modular! It's repeatable! It's got more features in the works (tag sorting, and order by... functionality are next on the docket)! It's connected to Firebase, and it's got a hidden admin portal (auth secured) that lets me easily add new images and has full CRUD functionality without having to edit any code, or directly access the database! Click on some pictures and play around with the navigation!
       </p>
       <p className="gal-blurb">
         If you're curious about my homebrew setting you can check out the lore{" "}
