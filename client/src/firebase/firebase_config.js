05101140a219ebf70b4d49a26f2098fabe19828f
habenzy <rstauss@burlingtoncodeacademy.com>
2023-06-01 10:12:33 -0400

    make it cleaner

--- client/src/firebase/firebase_config.js
+++ client/src/firebase/firebase_config.js
@@ -3,22 +3,14 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
 
-let local = {REACT_APP_FIREBASE_API_KEY: "AIzaSyDEUNCta2DoOpGsfjGq3ocG_PoVKD-EhoU",
-REACT_APP_FIREBASE_AUTH_DOMAIN: "personal-gallery-9243b.firebaseapp.com",
-REACT_APP_FIREBASE_PROJECT_ID: "personal-gallery-9243b",
-REACT_APP_FIREBASE_STOREAGE_BUCKET: "personal-gallery-9243b.appspot.com",
-REACT_APP_FIREBASE_MESSAGING_SENDER_ID: "1096920914936",
-REACT_APP_FIREBASE_APP_ID: "1:1096920914936:web:2f204fbbe865cf7c75540d",
-REACT_APP_FIREBASE_MEASUREMENT: "G-9510TCRTNR"}
-
 const app = firebase.initializeApp({
-  apiKey: local.REACT_APP_FIREBASE_API_KEY,
-  authDomain: local.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: local.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: local.REACT_APP_FIREBASE_STOREAGE_BUCKET,
-  messagingSenderId: local.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: local.REACT_APP_FIREBASE_APP_ID,
-  measurementId: local.REACT_APP_FIREBASE_MEASUREMENT
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STOREAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT
 });
 
 
